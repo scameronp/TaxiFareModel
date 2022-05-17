ee69d8791390b4378c11c0918e9744c7fdc7cb83
Sarah Cameron Pesant <cp.sarah@gmail.com>
2022-05-17 16:09:24 -0400

    Integration with MLFlow

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -3,10 +3,17 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LinearRegression
 from TaxiFareModel.utils import compute_rmse
+from TaxiFareModel.data import get_data, clean_data
 from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
+from memoized_property import memoized_property
+import mlflow
+from  mlflow.tracking import MlflowClient
 
 
 class Trainer():
+    MLFLOW_URI = "https://mlflow.lewagon.ai/"
+    EXPERIMENT_NAME = "[CA] [Montreal] [scameronp] LinearRegression + v1"
+
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -15,6 +22,28 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    def mlflow_create_run(self):
+	    self.mlfow_run = self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlfow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlfow_run.info.run_id, key, value)
+
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -49,8 +78,8 @@ class Trainer():
 
 
 if __name__ == "__main__":
-    # get data
-    # clean data
+    data = get_data()
+    clean_data = clean_data()
     # set X and y
     # hold out
     # train
